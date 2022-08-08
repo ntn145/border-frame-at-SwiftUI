62d4239a40373454fae9aa92b4f375923cd119da
Nhan Nguyen Trong <89365881+ntn145@users.noreply.github.com>
2022-08-08 20:55:36 +0700

    Initial Commit

---
+++ Day4/Day4App.swift
@@ -0,0 +1,17 @@
+//
+//  Day4App.swift
+//  Day4
+//
+//  Created by Khoai Alpha on 08/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Day4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
